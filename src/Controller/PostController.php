97d11b9c1d9aa51dc14d8abcd1ba8e45d05becaa
Andrasi <andrasi.m007@gmail.com>
2022-01-25 16:28:17 +0100

    update , delete post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -14,12 +15,18 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PostController extends AbstractController
 {
+    private $doctrine;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/post", name="post")
      */
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(): Response
     {
-        $posts = $doctrine->getRepository(Post::class)->findAll();
+        $posts = $this->doctrine->getRepository(Post::class)->findAll();
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
@@ -29,7 +36,7 @@ class PostController extends AbstractController
     /**
      * @Route("/post/new", name="post_new" , methods={"GET","POST"})
      */
-    public function new(Request $request, ManagerRegistry $doctrine, ValidatorInterface $validator)
+    public function new(Request $request, ValidatorInterface $validator)
     {
        $errorsString = [];
        $post = new Post();
@@ -52,7 +59,7 @@ class PostController extends AbstractController
             $post->setName($formData->getName());
             $post->setDescription($formData->getDescription());
 
-            $em = $doctrine->getManager();
+            $em = $this->doctrine->getManager();
             $em->persist($post);
             $em->flush();
 
@@ -61,6 +68,67 @@ class PostController extends AbstractController
 
 
         return $this->renderForm('post/new.html.twig', ['form' => $form, 'errors' => $errorsString ]);
+    }
+
+        /**
+         * @Route("/post/{id}", name="post_show")
+         */
+        public function show($id)
+        {
+            $post = $this->doctrine->getRepository(Post::class)->find($id);
+
+            return $this->render('post/show.html.twig', [
+                'post' => $post,
+            ]);
+        }
+
+    /**
+     * @Route("/post/{id}/delete", name="post_delete")
+     */
+    public function delete($id, PostRepository $postRepository)
+    {
+        $post = $postRepository->find($id);
+        $this->doctrine->getManager()->remove($post);
+        $this->doctrine->getManager()->flush();
+        $this->addFlash('success', 'Smazáno');
+        return $this->redirectToRoute('post');
+    }
+
+    /**
+     * @Route("/post/{id}/edit", name="post_edit")
+     */
+    public function edit(Post $post, Request $request, ValidatorInterface $validator)
+    {
+        $errorsString = [];
+
+        $form = $this->createFormBuilder($post)
+            ->add('name', TextType::class)
+            ->add('description', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Odeslat'])
+            ->getForm();
+
+
+        $form->handleRequest($request);
+        $errors = $validator->validate($post);
+
+        if (count($errors) > 0) {
+            $errorsString = $errors;
+        }
+
+        if ($form->isSubmitted() && $form->isValid() ) {
+            $formData = $form->getData();
+            $post->setName($formData->getName());
+            $post->setDescription($formData->getDescription());
+
+            $em = $this->doctrine->getManager();
+
+            $em->flush();
+
+            return $this->redirectToRoute('post');
+        }
+
+
+        return $this->renderForm('post/edit.html.twig', ['form' => $form, 'errors' => $errorsString ]);
     }
 
 }
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -29,6 +29,11 @@ class Post
      */
     private $description;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -54,6 +59,18 @@ class Post
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
